0bb55e03702e2e555f943d32e2b300694566a7f7
dragosg <drafffg@gmail.com>
2022-05-28 15:09:25 +0300

    Move functionality from main to dedicated modules

---
+++ utils.py
@@ -0,0 +1,47 @@
+import os
+
+
+import xmltodict
+from typing import Union
+
+
+def make_dir(dir_path: str) -> Union[str, None]:
+    """
+    Create a directory if it does not exist
+
+    :param dir_path: absolute path to the directory
+    :return: path to the directory if successful or None if and error occurred
+    """
+
+    try:
+        isdir = os.path.isdir(dir_path)
+        if not isdir:
+            os.mkdir(dir_path)
+        return dir_path
+
+    except Exception as error:
+        print(error)
+        return None
+
+
+def get_bbox_from_xml(path: str) -> list:
+    """
+    Get bounding boxes from xml files formatted in Pascal VOC format
+
+    :param path: Path to xml file
+    :return: list of bounding boxes
+    """
+
+    with open(path) as xml_file:
+        data_dict = xmltodict.parse(xml_file.read())
+
+    xml_file.close()
+    bounding_boxes = data_dict["annotation"]["object"]
+    bounding_boxes = [
+        {
+            'bndbox': bb["bndbox"],
+            'name': bb['name']
+        } for bb in bounding_boxes
+    ]
+
+    return bounding_boxes
